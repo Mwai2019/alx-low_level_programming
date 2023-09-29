c7e69aaf974aad3b08207e78a8f917f0cf77135c
mwai2019 <mwaimatembamk2019@gmail.com>
2023-09-29 12:50:00 -0700

    task

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -13,7 +13,6 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -27,5 +26,6 @@ char *_strncat(char *dest, char *src, int n)
 	j++;
 	}
 	dest[i] = '\0';
-	return
+	return (dest);
+}
 
